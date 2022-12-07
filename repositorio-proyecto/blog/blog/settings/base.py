ac00f09b6190532cab1aa1707b022abe1e04ed85
Santiago Nickisch <SantiNick28@gmail.com>
2022-12-07 12:30:13 -0300

    Avances de la pagina
    Cambiamos todo, empezamos usando una plantilla que sacamos del repositorio de git de un video que nos encontramos en YouTube, cambiamos todo literalmente.
    Dicha plantilla le adaptamos el header y sus dichos colores, le agregamos el logo de nuestra ONG al final de la pagina en el footer.

--- repositorio-proyecto/blog/blog/settings/base.py
+++ repositorio-proyecto/blog/blog/settings/base.py
@@ -108,6 +108,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
